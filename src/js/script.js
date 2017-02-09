a83e12e996a04c89818a546126677c0cb17593a6
Magomed Suleymanov <gamidovich@mail.ru>
2017-02-09 16:09:57 +0300

    index.html 98%

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,13 @@
-$(document).ready(function(){
-				window.onload = function() {
-					var toggler = document.getElementById('toggler');
-					toggler.onclick = function(e){
-					e.preventDefault();
-					toggler.classList.toggle('toggler--close');
-					document.getElementById('main-nav').classList.toggle('header-nav__list--visible');
-					}
-				}
-				loadFont(
-		'FiraSans', 'PTSerifRegular',                      // название шрифта
-		'/css/font.css?v=1'  // откуда с сервака тянуть CSS с закодированным в base64 форматом WOFF   // откуда с сервака тянуть CSS с закодированным в base64 форматом WOFF2
-		);
+$(document).ready(function() {
+
+	var toggler = document.getElementById('toggler');
+	var mainNav = document.getElementById('main-nav');
+
+	mainNav.classList.toggle('header-nav__list--close');
+
+	toggler.onclick = function(e){
+		e.preventDefault();
+		mainNav.classList.toggle('header-nav__list--visible');		
+	}	
+
 });
