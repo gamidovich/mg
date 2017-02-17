ca49158b132710259ae190c4f0a00b7f1d1fafd5
Magomed Suleymanov <gamidovich@mail.ru>
2017-02-17 22:11:04 +0300

    add js

--- src/js/script.js
+++ src/js/script.js
@@ -39,3 +39,55 @@ $(document).ready(function(){
 });
 
 });
+
+window.onload = function() {
+
+    var btn = document.getElementById('reviews-btn');
+
+    function getCoords(elem) {
+  		var box = elem.getBoundingClientRect();
+  		var bottom = box.bottom + pageYOffset;
+		return bottom;
+    }
+
+    var btnCoord = getCoords(btn);
+    var winY = document.documentElement.clientHeight;
+
+    window.onscroll = function() {
+  
+    	if(btnCoord <= winY) {
+    		console.log('I love Epixx' + ' ' + btnCoord + ' ' + winY);
+    	}
+  
+    }
+
+};
+
+// // 
+// window.onscroll = function() {
+
+
+// }
+
+//   // // опеределяем насколько прокручен документ
+//   // var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+//   // // получаем кнопку
+//   var btn = document.getElementById('reviews-btn');
+
+
+//   var btnCoord = btn.
+
+//   // координата элемента в документе
+//   function getCoords(elem) {
+
+//   	var box = elem.getBoundingClientRect();
+//   	var top = box.top + pageYOffset;
+
+// 	return top;
+
+//    }
+// console.log(getCoords(btn));
+//    //   if(scrolled >=  getCoords(btn)) {
+//   	// alert('sdsdsdsdsds');
+//   	// }
