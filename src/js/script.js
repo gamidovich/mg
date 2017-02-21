e121d1b9038ca444b91a02bab5596d1009afbdec
Magomed Suleymanov <gamidovich@mail.ru>
2017-02-21 11:14:50 +0300

    fix footer for ie11

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,39 @@
 $(document).ready(function() {
 
-	var toggler = document.getElementById('toggler');
-	var mainNav = document.getElementById('main-nav');
+  var toggler = document.getElementById('toggler');
+  var mainNav = document.getElementById('main-nav');
 
-	mainNav.classList.toggle('header-nav__list--close');
+  mainNav.classList.toggle('header-nav__list--close');
 
-	toggler.onclick = function(e){
-		e.preventDefault();
-		mainNav.classList.toggle('header-nav__list--visible');		
-	}	
+  toggler.onclick = function(e){
+    e.preventDefault();
+    mainNav.classList.toggle('header-nav__list--visible');    
+  } 
 });
 
 $(document).ready(function(){
 
-  	$('.slider-for').slick({
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  arrows: false,
-	  fade: true,
-	  asNavFor: '.slider-nav'
-	});
+    $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
 
-	$('.slider-nav').slick({
-	  slidesToShow: 6,
-	  slidesToScroll: 1,
-	  asNavFor: '.slider-for',
-	  centerMode: true,
-	  focusOnSelect: true,
-	  responsive: [{
+  $('.slider-nav').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    centerMode: true,
+    focusOnSelect: true,
+    responsive: [{
 
-      	breakpoint: 768,
-      		settings: {
-        	slidesToShow: 5,
-        	slidesToScroll: 1
-      	    }
+        breakpoint: 768,
+          settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1
+            }
 
       }]
 });
@@ -59,26 +59,26 @@ window.onload = function() {
     var btn = document.getElementById('reviews-btn');
 
     function getCoords(elem) {
-  		var box = elem.getBoundingClientRect();
-  		var bottom = box.bottom + pageYOffset;
-		return bottom;
+      var box = elem.getBoundingClientRect();
+      var bottom = box.bottom + pageYOffset;
+    return bottom;
     }
 
     var btnCoord = getCoords(btn);
 
     function winY() {
-    	return document.documentElement.clientHeight + pageYOffset;
+      return document.documentElement.clientHeight + pageYOffset;
     }
 
     if(btnCoord <= winY()) {
-    	btn.classList.add('btn--animation');	
+      btn.classList.add('btn--animation');  
     }
 
-    window.onscroll = function() {    	
+    window.onscroll = function() {      
   
-    	if(btnCoord <= winY()) {    		
-   			btn.classList.add('btn--animation');
-    	}
+      if(btnCoord <= winY()) {        
+        btn.classList.add('btn--animation');
+      }
   
     }
 
@@ -86,18 +86,18 @@ window.onload = function() {
     var imgLength = recImg.length;
 
     function changeImg(e) {
-    	var currentImg = e.target;
-    	for(var i = 0; i < imgLength; i++) {
-    		if(recImg[i] == currentImg) {
-    			recImg[i].classList.toggle('shadow');
-    		} else {
-    			recImg[i].classList.remove('shadow');
-    		}
-    	}
+      var currentImg = e.target;
+      for(var i = 0; i < imgLength; i++) {
+        if(recImg[i] == currentImg) {
+          recImg[i].classList.toggle('shadow');
+        } else {
+          recImg[i].classList.remove('shadow');
+        }
+      }
     }
 
     for(var j = 0; j < imgLength; j++) {
-  		recImg[j].addEventListener('click', changeImg);
-	}
+      recImg[j].addEventListener('click', changeImg);
+  }
 
 };
